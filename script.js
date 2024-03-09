cf1069eaed862fac397add75030212a08308c9bc
Julien Sailly <juliensailly76@gmail.com>
2024-03-09 17:05:57 +0100

    feat: add the maps search link but still some errors

---
+++ script.js
@@ -0,0 +1,18 @@
+function main() {
+    const interval = setInterval(() => {
+        const searchStats = document.querySelector('#result-stats')
+        if (searchStats) {
+            clearInterval(interval);
+            AddMapsButton(searchStats);
+        }
+    })
+    setTimeout(() => {
+        clearInterval(interval);
+    }, 5000);
+}
+
+function AddMapsButton(searchStats) {
+    searchStats.innerHTML += ' <a href="https://www.google.com/maps?q=' + window.location.href.split('q=')[1].split('&')[0] + '&hl=en&gl=us&shorturl=1" id="mapsButton">Rechercher sur Maps</a>';
+}
+
+main();
